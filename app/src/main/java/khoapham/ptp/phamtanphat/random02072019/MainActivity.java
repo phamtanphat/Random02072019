0683d5c58119c0bca07c717fa179e0e22be7baa9
Phạm Tấn Phát <phatdroid94@gmail.com>
2019-07-27 19:28:00 +0700

    a

--- app/src/main/java/khoapham/ptp/phamtanphat/random02072019/MainActivity.java
+++ app/src/main/java/khoapham/ptp/phamtanphat/random02072019/MainActivity.java
@@ -12,12 +12,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 //        Builtin function
-        int a = (int) Math.floor(Math.random()*5);
+//        int a = (int) Math.floor(Math.random()*5);
         //trần nhà : làm tròn lên
 //        int b = (int) Math.ceil(1.1);
         //sàn nhà : làm tròn xuống
 //        int b = (int) Math.floor(1.1);
-
-//        Log.d("BBB",b + "");
+        //Tìm số chính phương 0 - 100
+//        int canbac2 = (int) Math.sqrt(100);
+////        Log.d("BBB",b + "");
+//        Log.d("BBB",canbac2 + "");
+        //Bài toán kiểm tra số chính phương
+        for (int i = 0; i<= 100 ; i++){
+            if (Math.sqrt(i) % 1 == 0){
+                Log.d("BBB",i + "");
+            }
+        }
     }
 }
