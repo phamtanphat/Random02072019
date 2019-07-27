8b96acda3ec190282b6a9a1f6db4dea7b1bf7ff2
Phạm Tấn Phát <phatdroid94@gmail.com>
2019-07-27 19:13:33 +0700

    a

--- app/src/main/java/khoapham/ptp/phamtanphat/random02072019/MainActivity.java
+++ app/src/main/java/khoapham/ptp/phamtanphat/random02072019/MainActivity.java
@@ -3,6 +3,7 @@ package khoapham.ptp.phamtanphat.random02072019;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        Builtin function
+        int a = (int) Math.floor(Math.random()*5);
+        //trần nhà : làm tròn lên
+//        int b = (int) Math.ceil(1.1);
+        //sàn nhà : làm tròn xuống
+//        int b = (int) Math.floor(1.1);
+
+//        Log.d("BBB",b + "");
     }
 }
