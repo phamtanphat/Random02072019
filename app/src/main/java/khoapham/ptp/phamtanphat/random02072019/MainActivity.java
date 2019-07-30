95251aa29e51c4b4af0aae518453b93144d455da
Phạm Tấn Phát <phatdroid94@gmail.com>
2019-07-30 17:58:13 +0700

    a

--- app/src/main/java/khoapham/ptp/phamtanphat/random02072019/MainActivity.java
+++ app/src/main/java/khoapham/ptp/phamtanphat/random02072019/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -22,10 +24,23 @@ public class MainActivity extends AppCompatActivity {
 ////        Log.d("BBB",b + "");
 //        Log.d("BBB",canbac2 + "");
         //Bài toán kiểm tra số chính phương
-        for (int i = 0; i<= 100 ; i++){
-            if (Math.sqrt(i) % 1 == 0){
-                Log.d("BBB",i + "");
-            }
-        }
+//        for (int i = 0; i<= 100 ; i++){
+//            if (Math.sqrt(i) % 1 == 0){
+//                Log.d("BBB",i + "");
+//            }
+//        }
+//        Random random = new Random();
+//        for (int i = 0 ; i<20 ; i++){
+//            //(max - min + 1 ) + min : 5 - 45
+//            int number = random.nextInt(45 - 5 + 1) + 5;
+//            Log.d("BBB",number + "");
+//        }
+        // 1 : Random so min va so max
+//            + Không số random bị trùng
+//            + Không được random quá tổng số (100 số - 100 lần)
+//            + Thông báo khi hết số random
+        // 2 : Hiển thị
+//            + Kiểu 1 : 1- 5 - 7 - 8 -
+//            + Kiểu 2 : 1- 5 - 7 - 8
     }
 }
